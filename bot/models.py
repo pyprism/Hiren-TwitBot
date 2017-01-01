17e77d59ba5d905e78d87234b95cf97c6018bdf5
Ashutosh Das <git.pyprism@gmail.com>
2017-01-01 19:56:52 +0600

    Added model

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class TwitterApp(models.Model):
+    name = models.CharField(max_length=500)
+    consumer_key = models.CharField(max_length=1000)
+    consumer_secret = models.CharField(max_length=1000)
+    access_token = models.CharField(max_length=1000)
+    access_token_secret = models.CharField(max_length=1000)
+    tag = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Twit(models.Model):
+    app = models.ForeignKey(TwitterApp)
+    status = models.TextField()
+    status_id = models.CharField(max_length=1000)
+    approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
