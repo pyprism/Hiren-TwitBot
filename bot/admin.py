893cc2406de42383b59929f8b9de802b902ca5e6
Ashutosh Das <git.pyprism@gmail.com>
2017-01-01 19:56:42 +0600

    Added model

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import TwitterApp, Twit
 # Register your models here.
+
+
+class TwitterAppAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'tag']
+
+
+class TwitAdmin(admin.ModelAdmin):
+    list_display = ['id', 'app', 'approved']
+    ordering = ['approved']
+
+admin.site.register(TwitterApp, TwitterAppAdmin)
+admin.site.register(Twit, TwitAdmin)
